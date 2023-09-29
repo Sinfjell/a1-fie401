842f48ca2c203a12ecb0317fd8dc23580abd0fde
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2023-09-29 09:09:55 +0200

    initial commit

--- assignment-1.R
+++ assignment-1.R
@@ -1,5 +1,7 @@
 # Load the RData file
 load("CAR_M&A.RData")
+
+# Load libraries
 library(robustHD)
 library(dplyr)
 library(lmtest)
@@ -15,21 +17,33 @@ df <- get(loaded_names[1])
 # Now you can run summary statistics
 summary(df)
 
-# Boxplot to check for outliers in multiple variables
-boxplot(df[, c("deal.value", "bidder.size", "bidder.car", "bidder.mtb", "bidder.runup", "bidder.fcf", "bidder.lev", "deal.relsize")],
-        main="Boxplot for Outliers",
-        las=2,  # makes the axis labels perpendicular to the axis
-        cex.axis=0.8)  # reduces the size of the axis labels
+# Create an empty list to store the boxplots
+boxplot_list <- list()
 
-# Create separate boxplots for variables on different scales
-par(mfrow=c(2,1))  # divide the plotting area into 2 rows and 1 column
-boxplot(df[, c("deal.value", "bidder.size")], main="Boxplot for Large Scale Variables", las=2, cex.axis=0.8)
-boxplot(df[, c("bidder.car", "bidder.mtb", "bidder.runup", "bidder.fcf", "bidder.lev", "deal.relsize")], main="Boxplot for Small Scale Variables", las=2, cex.axis=0.8)
+# Set up the plotting area for multiple plots
+par(mfrow = c(2, 2))  # 4 rows and 2 columns
 
+# List of variables to plot
+variables <- c("bidder.car", "deal.value", "bidder.size", "bidder.mtb", "bidder.runup", "bidder.fcf", "bidder.lev", "bidder.sigma", "deal.relsize")
 
+# Loop through each variable to create a boxplot
+for (var in variables) {
+  boxplot(df[[var]], main = paste("Boxplot of", var), ylab = var)
+  boxplot_list[[var]] <- recordPlot()  # Store the boxplot
+}
 
 # Winsorize the data (if needed)
 df$bidder.size <- winsorize(df$bidder.size, probs = c(0.01, 0.99))
+
+# List of variables to Winsorize
+variables <- c("bidder.car", "deal.value", "bidder.size", "bidder.mtb", "bidder.runup", "bidder.fcf", "bidder.lev", "bidder.sigma", "deal.relsize")
+
+# Loop through each variable to Winsorize
+for (var in variables) {
+  df[[var]] <- winsorize(df[[var]], probs = c(0.01, 0.99))
+}
+
+
 
 
 # Bullet 2 ----------------------------------------------------------------
